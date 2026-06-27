a4097e412a07da1227b1aa6910bca1a95aa95a10
Bryan Benedict Bangun <bryan180505@gmail.com>
2026-06-27 22:55:04 +0700

    Initial Laravel project setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>MotoCare</title>
+</head>
+<body>
+    <h1>MotoCare</h1>
+    <p>Sistem monitoring perawatan motor berbasis kilometer.</p>
+</body>
+</html>
